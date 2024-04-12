179af2626c2aa6cf76ee50db2eb2b620fae7641b
johnnguyenit39 <johnnguyen.it.39@gmail.com>
2024-04-12 23:00:00 +0700

    update code

--- lib/interactable_svg/src/widgets/region_painter.dart
+++ lib/interactable_svg/src/widgets/region_painter.dart
@@ -10,6 +10,7 @@ class RegionPainter extends CustomPainter {
   final List<Region> selectedRegion;
   final Color? strokeColor;
   final Color? selectedColor;
+  final Color? fillColor;
   final Color? dotColor;
   final double? strokeWidth;
   final bool? centerDotEnable;
@@ -26,6 +27,7 @@ class RegionPainter extends CustomPainter {
     required this.region,
     required this.selectedRegion,
     this.selectedColor,
+    this.fillColor,
     this.strokeColor,
     this.dotColor,
     this.centerDotEnable,
@@ -47,10 +49,8 @@ class RegionPainter extends CustomPainter {
       ..color = selectedColor ?? Colors.blue
       ..strokeWidth = 1.0
       ..style = PaintingStyle.fill;
-
     final colorFill = Paint()
-      ..color = Colors.grey
-      ..strokeWidth = 1.0
+      ..color = fillColor ?? Colors.grey
       ..style = PaintingStyle.fill;
 
     final redDot = Paint()
@@ -62,8 +62,9 @@ class RegionPainter extends CustomPainter {
 
     _scale = sizeController.calculateScale(size);
     canvas.scale(_scale);
-
-    canvas.drawPath(region.path, colorFill);
+    if (fillColor != null) {
+      canvas.drawPath(region.path, colorFill);
+    }
 
     if (selectedRegion.contains(region)) {
       canvas.drawPath(region.path, selectedPen);
